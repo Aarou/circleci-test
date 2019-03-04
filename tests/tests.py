30b57ec36c1c4e312c857147e0fb39143c8154a2
ltetrel <loic.tetrel.pro@gmail.com>
2019-03-04 17:36:20 -0500

    * modified tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,22 +9,25 @@ Created on Mon Mar  4 16:26:45 2019
 import unittest
 from tools import dirhash
 import urllib.request as ulib
-import urllib.parse as urlparse
-import shutil
 import tarfile
+import filecmp
 
 def download():
-    (tmpfile, headers) = ulib.urlretrieve("https://github.com/SIMEXP/Repo2Data/archive/multiple_data_requirement.tar.gz", "master.tar.gz")
-#    (tmpfile, headers) = ulib.urlretrieve("https://github.com/SIMEXP/Repo2Data/archive/master.tar.gz", "master.tar.gz")
+#    (tmpfile, headers) = ulib.urlretrieve("https://github.com/SIMEXP/Repo2Data/archive/multiple_data_requirement.tar.gz", "master.tar.gz")
+    (tmpfile, headers) = ulib.urlretrieve("https://github.com/SIMEXP/Repo2Data/archive/master.tar.gz", "master.tar.gz")
     tar = tarfile.open("master.tar.gz", "r:gz")
     tar.extractall()
     tar.close()
 
 class Test(unittest.TestCase):
 
-    def test_equal(self):
+    def test_names_equal(self):
         download()
-        self.assertEqual(dirhash("Repo2Data-multiple_data_requirement"), dirhash("./test_in/Repo2Data-master"))
+        self.assertFalse(filecmp.dircmp("Repo2Data-master", "./test_in/Repo2Data-master").diff_files)
+        
+    def test_content_equal(self):
+        download()
+        self.assertEqual(dirhash("Repo2Data-master"), dirhash("./test_in/Repo2Data-master"))
         
 if __name__ == '__main__':
     unittest.main()
