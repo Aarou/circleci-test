7630b1e988157d814da8032197a6fc8c7818c277
ltetrel <loic.tetrel.pro@gmail.com>
2019-03-07 11:55:40 -0500

    * now with simple operations..

---
+++ operations/operations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 11:52:07 2019
+
+@author: ltetrel
+"""
+
+def addition(a, b):
+    return a + b
+
+def sup(a, b):
+    return a > b
